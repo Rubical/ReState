f9ff382845953cefbe3854f366b18f8a11043e88
Rubical <zhpiv96@mail.ru>
2024-12-26 18:10:40 +0300

    firs commit

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { SafeAreaView, Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5"></View>
+    </SafeAreaView>
+  );
+}
